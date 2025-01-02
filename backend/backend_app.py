9bb3101b3020f3957b744a14bd5de73fd8da0b30
farshidmousavi <sdfarshidmousavi@gmail.com>
2025-01-03 02:11:06 +0330

    Implement add new post

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,4 +1,6 @@
-from flask import Flask, jsonify
+import json
+
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -13,6 +15,45 @@ POSTS = [
 @app.route('/api/posts', methods=['GET'])
 def get_posts():
     return jsonify(POSTS)
+
+
+@app.route("/api/posts", methods=["POST"])
+def add_post():
+    data = request.get_json()
+
+    error_response = _validate_add_post(data)
+    if error_response:  # If there's an error, return it
+        return error_response
+
+    new_id = get_new_id(POSTS)
+
+    new_post = {
+        "id": new_id,
+        "title": data.get("title"),
+        "content": data.get("content")
+    }
+    POSTS.append(new_post)
+
+    return jsonify(new_post), 201
+
+
+def _validate_add_post(data: dict):
+    missing_fields = []
+    if not data.get("title"):
+        missing_fields.append("title")
+    if not data.get("content"):
+        missing_fields.append("content")
+
+    if missing_fields:
+        return jsonify({
+            "error": "Bad Request",
+            "message": f"Missing fields: {', '.join(missing_fields)}"
+        }), 400
+    return None  # No errors
+
+
+def get_new_id(posts) -> int:
+    return max([post["id"] for post in posts], default=0) + 1
 
 
 if __name__ == '__main__':
